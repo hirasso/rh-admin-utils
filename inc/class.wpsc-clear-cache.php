b8fa6709a0cd8efa2c44a05a16b57ce43face270
Rasso Hilber <mail@rassohilber.com>
2022-01-25 13:39:49 +0100

    Clear WP Super Cache when updating ACF options pages

--- inc/class.wpsc-clear-cache.php
+++ inc/class.wpsc-clear-cache.php
@@ -8,6 +8,7 @@ class WpscClearCache extends Singleton {
 
   public function __construct() {
     add_action('admin_init', [$this, 'wp_super_cache_init']);
+    add_action('acf/save_post', [$this, 'acf_save_post']);
   }
 
   /**
@@ -19,19 +20,19 @@ class WpscClearCache extends Singleton {
     if( is_plugin_active('wp-super-cache/wp-cache.php') ) {
       add_action('admin_bar_menu', [$this, 'replace_wp_super_cache_admin_bar_button'], 999 );
     }
-    if( intval($_GET['rh_clear_cache'] ?? null) === 1 ) $this->clear_cache();
+    if( intval($_GET['rh_clear_cache'] ?? null) === 1 ) $this->clear_cache_and_redirect();
   }
 
   /**
-   * Deletes the cache
+   * Deletes the cache and redirects to referrer
    *
    * @return void
    */
-  private function clear_cache() {
+  private function clear_cache_and_redirect() {
     global $cache_path;
     check_admin_referer( 'rh_clear_cache' );
-    prune_super_cache( $cache_path . 'supercache/', true );
-    prune_super_cache( $cache_path, true );
+
+    $this->clear_cache();
 
     do_action('rh/wpsc-cc/clear_cache');
 
@@ -75,5 +76,30 @@ class WpscClearCache extends Singleton {
 
     $wp_admin_bar->add_menu($args);
 
+  }
+
+  /**
+   * Clears WP Super Cache cache directory on acf save post of options pages
+   *
+   * @see https://support.advancedcustomfields.com/forums/topic/clear-wp-super-cache-on-update/
+   * 
+   * @param mixed $post_id
+   * @return void
+   * @author Rasso Hilber <mail@rassohilber.com>
+   */
+  public function acf_save_post($post_id): void {
+    if( is_string($post_id) ) $this->clear_cache();
+  }
+
+  /**
+   * Calls the WP Super Cache API function to clear the full cache directory
+   *
+   * @return void
+   * @author Rasso Hilber <mail@rassohilber.com>
+   */
+  private function clear_cache(): void {
+    // bail early if wp super cache isn't installed
+    if( !function_exists('wp_cache_clear_cache') ) return;
+    wp_cache_clear_cache();
   }
 }
