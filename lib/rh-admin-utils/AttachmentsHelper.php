f869981f4dfef6b12f25287fe4db6af4ef9eb7c6
Rasso Hilber <mail@rassohilber.com>
2023-03-20 13:11:04 +0100

    Also allow searching for `id:12345` on list view of `upload.php`

--- lib/rh-admin-utils/AttachmentsHelper.php
+++ lib/rh-admin-utils/AttachmentsHelper.php
@@ -20,48 +20,40 @@ class AttachmentsHelper
     public static function init()
     {
         add_filter('ajax_query_attachments_args', [__CLASS__, 'ajax_query_attachments_args']);
+        add_action('pre_get_posts', [__CLASS__, 'pre_get_posts']);
     }
 
     /**
-     * Hook into `ajax_query_attachments_args`
+     * Allow searching for "id:12345" on upload.php list view
+     *
+     * @param \WP_Query $query
+     * @return void
+     */
+    public static function pre_get_posts(\WP_Query $query): void
+    {
+        global $pagenow;
+        if (!$query->is_main_query()) return;
+        if (!is_admin()) return;
+        if ($pagenow !== 'upload.php') return;
+
+        if (!$post = self::get_post_from_searchstring($query->get('s') ?: '')) return;
+
+        $query->is_search = false;
+        $query->set('s', '');
+        $query->set('post__in', [$post->ID]);
+    }
+
+    /**
+     * Allow searching for "id:12345" on upload.php grid view
      *
      * @param array $args
      * @return array
      */
     public static function ajax_query_attachments_args(array $args): array
     {
-        $args = self::support_searching_for_attachment_id($args);
-        return $args;
-    }
+        $post = self::get_post_from_searchstring($args['s'] ?? null);
 
-    /**
-     * Allows to search for an attachment ID
-     *
-     * @param array $args
-     * @return array
-     */
-    private static function support_searching_for_attachment_id(array $args): array
-    {
-        $search_string = $args['s'] ?? null;
-
-        if (!$search_string) return $args;
-
-        /**
-         * Match search strings starting in the shape of
-         *
-         * - "id:12345"
-         * - "Id:12345"
-         * - "ID:12345"
-         *
-         */
-        if (!preg_match('/(?<=^id:)(?P<id>\d.+?)(?=\D|$)/i', $search_string, $matches)) return $args;
-
-        // Activate to debug
-        // wp_send_json_success(['matches' => $matches]);
-
-        if (!$post = get_post(intval($matches['id']))) return $args;
-
-        if ($post->post_type !== 'attachment') return $args;
+        if (!$post) return $args;
 
         // Convert $args to disable the search and instead return the matched post
         $args['post__in'] = [$post->ID];
@@ -69,5 +61,26 @@ class AttachmentsHelper
         unset($args['s']);
 
         return $args;
+    }
+
+    /**
+     * Try to find a matching post for search strings matching this pattern
+     *
+     * - "id:12345"
+     * - "Id:12345"
+     * - "ID:12345"
+     */
+    private static function get_post_from_searchstring(?string $search_string): ?\WP_Post
+    {
+        if (!$search_string) return null;
+
+        if (!preg_match('/(?<=^id:)(?P<id>\d.+?)(?=\D|$)/i', $search_string, $matches)) return null;
+
+        // Activate to debug
+        // wp_send_json_success(['matches' => $matches]);
+
+        if (!$post = get_post(intval($matches['id']))) return null;
+
+        return $post;
     }
 }
