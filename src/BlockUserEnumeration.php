4c3a8d3a4cd307395939d8d2141bc1db27b76485
Rasso Hilber <mail@rassohilber.com>
2026-03-09 20:20:20 +0100

    Make `BlockUserEnumeration` customizable via filter

---
+++ src/BlockUserEnumeration.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace RH\AdminUtils;
+
+class BlockUserEnumeration
+{
+    public static function init(): void
+    {
+        add_action('pre_get_posts', self::disableAuthorArchives(...));
+        add_filter('rest_endpoints', self::blockRestUsersEndpoint(...));
+    }
+
+    private static function isEnabled(): bool
+    {
+        return (bool) apply_filters('rhau/block_user_enumeration', true);
+    }
+
+    /**
+     * Disable author archives without giving away that they exist (404 vs. 301/2)
+     *
+     * The following command should return a 404:
+     * `curl -IL https://schulz-und-schulz.test/?author=1`
+     */
+    private static function disableAuthorArchives(\WP_Query $query): void
+    {
+        if (!self::isEnabled()) {
+            return;
+        }
+
+        if (is_admin() || !$query->is_main_query()) {
+            return;
+        }
+
+        if ($query->is_author() || isset($_GET['author'])) {
+            $query->set_404();
+            status_header(404);
+        }
+    }
+
+    /**
+     * Completely remove the WP JSON users endpoints
+     *
+     * This must return "rest_no_route": `curl -s https://example.com/wp-json/wp/v2/users | jq`
+     */
+    private static function blockRestUsersEndpoint(array $endpoints): array
+    {
+        if (!self::isEnabled()) {
+            return $endpoints;
+        }
+
+        if (current_user_can('list_users')) {
+            return $endpoints;
+        }
+
+        return array_filter(
+            $endpoints,
+            fn (string $key) => ! str_starts_with($key, '/wp/v2/users'),
+            ARRAY_FILTER_USE_KEY
+        );
+    }
+}
