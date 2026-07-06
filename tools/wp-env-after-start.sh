a2fe3c43c852a668ebf3b7472137da1af01380c0
Rasso Hilber <mail@rassohilber.com>
2026-07-06 11:12:32 +0200

    Automatically map installed plugins into `@wordpress/env`

--- tools/wp-env-after-start.sh
+++ tools/wp-env-after-start.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e
 
-pnpm run env:cli wp theme activate twentytwentyfive
-pnpm run env:cli wp rewrite structure '/%postname%/' --hard
-pnpm run env:cli wp plugin activate --all
+pnpm run env-cli wp theme activate twentytwentyfive
+pnpm run env-cli wp rewrite structure '/%postname%/' --hard
+pnpm run env-cli wp plugin activate --all
 
-pnpm run env:tests-cli wp theme activate twentytwentyfive
-pnpm run env:tests-cli wp rewrite structure '/%postname%/' --hard
-pnpm run env:tests-cli wp plugin activate --all
+pnpm run env-tests-cli wp theme activate twentytwentyfive
+pnpm run env-tests-cli wp rewrite structure '/%postname%/' --hard
+pnpm run env-tests-cli wp plugin activate --all
