eacabfe35179566c394588b2765b2d898e908996
Rasso Hilber <mail@rassohilber.com>
2026-08-18 10:28:07 +0200

    Allways allow file mods for the automatic updater
    This allows hardening Bedrock-manged sites via `DISALLOW_FILE_MODS` without loosing security patches from WP core

---
+++ .changeset/petite-rice-cry.md
@@ -0,0 +1,5 @@
+---
+"rh-admin-utils": patch
+---
+
+Allways allow file mods for the automatic updater, even if `DISALLOW_FILE_MODS` is `true`
--- src/Hardening/Hardening.php
+++ src/Hardening/Hardening.php
@@ -11,9 +11,23 @@ final class Hardening
     {
         add_filter('xmlrpc_enabled', '__return_false');
 
+        add_filter('file_mod_allowed', self::file_mod_allowed(...), 10, 2);
+
         UserEnumeration::init();
         HardenHtaccess::init();
         ObfuscateVersion::init();
         TrustedAdmins::init();
+    }
+
+    /**
+     * Allways allow file mods for the automatic updater,
+     * even if DISALLOW_FILE_MODS is true.
+     */
+    private static function file_mod_allowed(bool $allowed, string $context): bool
+    {
+        if ($context === 'automatic_updater') {
+            return true;
+        }
+        return $allowed;
     }
 }
